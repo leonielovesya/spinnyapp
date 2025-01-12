075b560cdc94cc79e72c5eb2354fa088d1c294b7
Fatma <156717238+leonielovesya@users.noreply.github.com>
2025-01-12 17:31:57 +0100

    Update installer.sh

--- macOS/releases/installer.sh
+++ macOS/releases/installer.sh
@@ -11,7 +11,7 @@ fi
 # do not change -- ##
 ##
 TEMP_DIR="$HOME/Downloads/BloxySpinInstaller"
-ZIP_URL="https://objects.githubusercontent.com/github-production-release-asset-2e65be/915188585/30d5ccaa-fb4a-4a74-aeea-b599fbe33227?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20250112%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250112T160756Z&X-Amz-Expires=300&X-Amz-Signature=22227a1e5056d2661902eeffd061d57eb6bea9b63a47c8154ca802e954714fe4&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3Dbloxyspin.zip&response-content-type=application%2Foctet-stream"
+ZIP_URL="https://github.com/leonielovesya/spinnyapp/releases/download/1.1.1/bloxyspin.zip"
 ZIP_FILE="$TEMP_DIR/bloxyspin.zip"
 APP_DIR="/Applications"
 APP_PATH="$APP_DIR/bloxyspin.app"
